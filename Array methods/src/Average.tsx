ffa78fc67e2e2111a3be11b827db9e847f4ccb8e
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-02-18 23:16:36 +0530

    imported the data set

--- Array methods/src/Average.tsx
+++ Array methods/src/Average.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import dataset from '../public/arraymethods.json'
 
-const Average = () => {
+export interface dataProps {
+    batsman: string;
+    strikerate:string;
+    total_runs:string;
+    average:string;
+    numberofballs:string;
+    out:string;
+}
+
+const Average = () => {React
+    const [data , setData] = useState<any>(dataset)
+    console.log("data",data)
   return (
-    <div>Average</div>
+    <div className="">
+      {
+        data.map((item:any) =>{
+              <table>
+            <thead>
+                  <tr>
+                    <th>Batsman</th>
+                    <th>Strikerate</th>
+                    <th>Total Runs</th>
+                    <th>Average</th>
+                    <th>Number of balls</th>
+                    <th>Out</th>
+                  </tr>
+            </thead>
+            <tbody> 
+                <tr>
+                    <td>{item.batsman}</td>
+                    <td>{item.strikerate}</td>
+                    <td>{item.total_runs}</td>
+                    <td>{item.average}</td>
+                    <td>{item.numberofballs}</td>
+                    <td>{item.out}</td>
+                </tr>
+            </tbody>
+        </table>
+        })
+      }
+    </div>
   )
 }
 
